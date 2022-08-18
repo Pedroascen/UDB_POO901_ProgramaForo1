513935aff17a3b297abf2a94473b45f59a4772af
Pedroascen <pedroascen3@gmail.com>
2022-08-17 21:15:25 -0600

    Se implemento opciones 1,2,3 faltan opcion 4 y validaciones

--- src/udb_virtual_poo/UDB_VIRTUAL_POO.java
+++ src/udb_virtual_poo/UDB_VIRTUAL_POO.java
@@ -1,9 +1,15 @@
 package udb_virtual_poo;
 
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class UDB_VIRTUAL_POO {
-        static String carnet,nombre;
+        //variables estaticas
+        static String carnet,nombre,buscar;
+        static  HashMap<String, String> EstudiantesMap = new HashMap<>();
     public static void main(String[] args) {
         //variables de entrada
         Scanner entrada = new Scanner(System.in);
@@ -83,29 +89,54 @@ public class UDB_VIRTUAL_POO {
                 System.exit(0);
             break;
         default: System.err.println("Opción inválida.");
+            break;
         }}while(opcion!=5);
     }
     
     //metodos
     public static void IngresarRegistro(){
-        System.out.println("Seleccionó la opción 1");
+        System.out.println("Seleccionó la opción de ingresar registro.");
         //variables de entrada
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese el carnet del alumno:");
         carnet = entrada.next();
         System.out.println("Ingrese el nombre del alumno:");
         nombre = entrada.next();
-        System.out.println("El valor del carnet es: "+carnet);
-        System.out.println("El valor del nombre es: "+nombre);
-
+        
+         //Agregando estudiante al Hashmap
+        EstudiantesMap.put(carnet,nombre);
+        //Imprimiendo mensaje
+         System.out.println("Alumno ingresado exitosamente\r");
+         System.out.println("El valor del carnet es: "+carnet);
+         System.out.println("El valor del nombre es: "+nombre);
     }
     
     public static void FiltrarRegistro(){
-        System.out.println("Seleccionó la opción 2");
+          System.out.println("Seleccionó la opción de filtrar registro.");
+        Scanner entrada = new Scanner(System.in);
+        //Variables de entrada
+         System.out.println("Por favor ingrese el carnet del alumno para buscar:");
+         buscar = entrada.nextLine();
+         
+         if(EstudiantesMap.containsKey(buscar))
+         {
+             System.out.printf("Se encontró al alumno " +EstudiantesMap.get(buscar)+ " con el carnet " +buscar+".\n");
+         }
+         else
+         {
+             System.out.println("Alumno no encontrado, no se puede mostrar.\n");
+         }
     }
     
     public static void ListarRegistro(){
-        System.out.println("Seleccionó la opción 3");
+        System.out.println("Seleccionó la opción de listar registros");
+        System.out.println("Listando alumnos registrados:");
+        Set set = EstudiantesMap.entrySet();
+        Iterator iterador = set.iterator();
+        while (iterador.hasNext()) {
+            Map.Entry metry = (Map.Entry)iterador.next();
+            System.out.println("Carnet: "+metry.getKey()+" - Nombre: "+metry.getValue());
+        }
     }
     
     public static void EliminarRegistro(){
