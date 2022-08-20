c33adcf9b41a43336250824e873827c6951ade32
Pedroascen <pedroascen3@gmail.com>
2022-08-19 20:40:49 -0600

    Se valido el menu y se modificaron las opciones

--- src/udb/virtual/poo/ejercicios/EjemploList.java
+++ src/udb/virtual/poo/ejercicios/EjemploList.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package udb.virtual.poo.ejercicios;
 
-/**
- *
- * @author Ascencio
- */
+import java.util.*;
+
 public class EjemploList {
-    
+
+    public static void main(String[] args) {
+        //List 
+        //¿Como se declara el list?
+        //Se declaran utilizando la palabra clave list, por ejemplo list.add
+        System.out.println("¿Como se declaran?");
+        List<String> lista = new ArrayList();
+        lista.add("Obama");
+        lista.add("Putin");
+        lista.add("Biden\n");
+
+        for(String elemento : lista) {
+            System.out.println("" + elemento);
+        }
+        //¿Como se asignan valores ? Utilizando  list = new ArrayList<>(); Ejemplo
+        System.out.println("¿Como se asignan valores?");
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        System.out.println(list+"\n");
+
+        //¿Como se elimina valores utilizando el mètodo remove Ejemplo MyStack.remove("Mango")
+        System.out.println("¿Como se elimina valores utilizando el mètodo remove?");
+        Stack myStack = new Stack();
+        myStack.add("Mango");
+        myStack.remove("Sandia");
+        myStack.add("Pera");
+        System.out.println("Fruta favorita: " + myStack);
+
+    }
 }
--- src/udb/virtual/poo/programa/UDB_VIRTUAL_POO.java
+++ src/udb/virtual/poo/programa/UDB_VIRTUAL_POO.java
@@ -1,7 +1,6 @@
 package udb.virtual.poo.programa;
 
-//Aplicacion para registrar alumnos con interfaz Map
-import java.awt.TextField;
+//Aplicacion para registrar alumnos usando Map
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Iterator;
@@ -11,21 +10,20 @@ import java.util.Set;
 public class UDB_VIRTUAL_POO {
         //variables de tipo estaticas
         static String carnet,nombre,buscar;
-        //Declaracion del HashMap
         static  HashMap<String, String> EstudiantesMap = new HashMap<>();
     public static void main(String[] args) {
         //Datos de entrada con Scanner
         Scanner entrada = new Scanner(System.in);
-        String num; 
+        String num;
         int opcion;
         
         do{
         //Menú de consola
         System.out.println("***************************************");
-        System.out.print("      Bienvenido a UDB Virtual\n");
+        System.out.print("       Bienvenido a UDB Virtual\n");
         System.out.print("1- Ingresar alumno.\n");
         System.out.print("2- Buscar alumno.\n");
-        System.out.print("3- Mostrar alumnos.\n");
+        System.out.print("3- Mostrar todos los alumnos.\n");
         System.out.print("4- Eliminar alumno.\n");
         System.out.print("5- Salir.\n");
         System.out.print("Por favor, ingrese una opción del menú:\n");
@@ -51,7 +49,7 @@ public class UDB_VIRTUAL_POO {
                 System.out.print("Digite cualquier número para continuar u opción 5 para salir.\n");
                 opcion = entrada.nextInt();
                 if(opcion==5){
-                    System.out.println("Gracias por usar el programa!!!");
+                    System.out.println("¡Gracias por usar el programa!");
                     System.exit(0);
                 }
             break;
@@ -64,7 +62,7 @@ public class UDB_VIRTUAL_POO {
                 System.out.print("Digite cualquier número para continuar u opción 5 para salir.\n");
                 opcion = entrada.nextInt();
                 if(opcion==5){
-                    System.out.println("Gracias por usar el programa!!!");
+                    System.out.println("¡Gracias por usar el programa!");
                     System.exit(0);
                 }
             break;
@@ -77,7 +75,7 @@ public class UDB_VIRTUAL_POO {
                 System.out.print("Digite cualquier número para continuar u opción 5 para salir.\n");
                 opcion = entrada.nextInt();
                 if(opcion==5){
-                    System.out.println("Gracias por usar el programa!!!");
+                    System.out.println("¡Gracias por usar el programa!");
                     System.exit(0);
                 }
             break;
@@ -90,12 +88,12 @@ public class UDB_VIRTUAL_POO {
                 System.out.print("Digite cualquier número para continuar u opción 5 para salir.\n");
                 opcion = entrada.nextInt();
                 if(opcion==5){
-                    System.out.println("Gracias por usar el programa!!!");
+                    System.out.println("¡Gracias por usar el programa!");
                     System.exit(0);
                 }
             break;
             case 5:
-                System.out.println("Gracias por usar el programa!!!");
+                System.out.println("¡Gracias por usar el programa!");
                 System.exit(0);
             break;
         default: System.err.println("Opción inválida.");
@@ -103,34 +101,29 @@ public class UDB_VIRTUAL_POO {
         }}while(opcion!=5);
     }
     
-    //metodo para validar opcion
-    public boolean Validar(){
-            return false;
-    }
-    
     //metodo para ingresar registro
     public static void IngresarRegistro(){
-        System.out.println("Seleccionó la opción de ingresar registro.");
+        System.out.println("Seleccionó la opción de ingresar alumno.");
         //variables de entrada
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese el carnet del alumno:");
-        carnet = entrada.next();
-        System.out.println("Ingrese el nombre del alumno:");
-        nombre = entrada.next();
+        carnet = entrada.nextLine();
+        System.out.println("Ingrese el nombre completo del alumno:");
+        nombre = entrada.nextLine();
         
          //Agregando estudiante al Hashmap
         EstudiantesMap.put(carnet,nombre);
         //Imprimiendo mensaje
          System.out.println("Alumno ingresado exitosamente\r");
-         System.out.println("El valor del carnet es: "+carnet);
-         System.out.println("El valor del nombre es: "+nombre);
+         System.out.println("Carnet: "+carnet);
+         System.out.println("Nombre completo: "+nombre);
     }
     //metodo para buscar registro
     public static void FiltrarRegistro(){
-        System.out.println("Seleccionó la opción de filtrar registro.");
+        System.out.println("Seleccionó la opción de buscar alumno.");
         Scanner entrada = new Scanner(System.in);
 
-         System.out.println("Por favor ingrese el carnet del alumno para buscar:");
+         System.out.println("Por favor ingrese el carnet del alumno a buscar:");
          buscar = entrada.nextLine();
          //condicional de salida
          if(EstudiantesMap.containsKey(buscar))
@@ -144,14 +137,14 @@ public class UDB_VIRTUAL_POO {
     }
     //metodo para listar registros
     public static void ListarRegistro(){
-        System.out.println("Seleccionó la opción de listar registros");
+        System.out.println("Seleccionó la opción de mostrar todos los alumnos.");
         System.out.println("Listando alumnos registrados:");
         //metodo set, para encapsular valores
         Set set = EstudiantesMap.entrySet();
         //iterador para recorrer objeto set
         Iterator iterador = set.iterator();
         if(iterador.hasNext()==false){
-            System.out.println("No se encontraron registros...");
+            System.out.println("No se encontraron registros.");
         }
         while (iterador.hasNext()) {
             Map.Entry metry = (Map.Entry)iterador.next();
@@ -160,7 +153,7 @@ public class UDB_VIRTUAL_POO {
     }
     //metodo para eliminar registro
     public static void EliminarRegistro(){
-        System.out.println("Seleccionó la opción de eliminar registro");
+        System.out.println("Seleccionó la opción de eliminar alumno.");
         System.out.println("Por favor, ingrese el carnet del alumno a eliminar:");
         Scanner entrada = new Scanner(System.in);
         buscar = entrada.nextLine();
